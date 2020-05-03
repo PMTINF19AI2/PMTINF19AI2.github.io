a6fd70c32f7f49585ba855d9474713ea137e7fe6
silaswes <silas.wessely@gmx.de>
2020-05-03 22:33:31 +0200

    test

--- app.js
+++ app.js
@@ -4,39 +4,58 @@ let container;
 let camera;
 let renderer;
 let scene;
-let model;
+let house;
 
 function init() {
-    container = document.querySelector('.scene');
-   
-    //Create scene
+  container = document.querySelector(".scene");
 
-    scene = new THREE.Scene();
-    
-    const fov = 35;
-    const aspect = container.clientWidth / container.clientHeight;
-    const near = 0.1;
-    const far = 500;
-    
+  //Create scene
+  scene = new THREE.Scene();
 
-    //Camera Setup
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 30);
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
 
-    //Renderer
-    renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
+  //Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 5, 30);
 
-    container.appendChild(renderer.domElement);
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+  scene.add(ambient);
 
-    //Load Model
-    let loader = new THREE.GLTFLoader();
-    loader.load("./xbox_controller/scene.gltf", function(gltf) {
-        scene.add(gltf.scene);
-        renderer.render(scene, camera);
-    });
-    //test
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(50, 50, 100);
+  scene.add(light);
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Model
+  let loader = new THREE.GLTFLoader();
+  loader.load("./house/scene.gltf", function(gltf) {
+    scene.add(gltf.scene);
+    house = gltf.scene.children[0];
+    animate();
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  house.rotation.z += 0.005;
+  renderer.render(scene, camera);
 }
 
 init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
