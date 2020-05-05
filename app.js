46ab3db2c5470c57cb8868b59e9b3f4b8c2b261e
silaswes <silas.wessely@gmx.de>
2020-05-05 15:59:13 +0200

    bilddrehung rückgängig

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ const navSlide = () => {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
-                $('.sidebar').css('z-index',1);
             } else {
-                $('.sidebar').css('z-index',0);
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
